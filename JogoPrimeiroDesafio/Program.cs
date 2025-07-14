62686bfd37e9a786e6c9d07349ab44a29c9f65e8
thalles.bragatti <thalles.bragatti@VI-2407>
2025-07-14 13:48:29 -0300

    Adicionar arquivos de projeto.

---
+++ JogoPrimeiroDesafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoPrimeiroDesafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
